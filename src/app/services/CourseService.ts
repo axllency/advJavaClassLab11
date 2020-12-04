8dc88167ac642b1a5f1e3a2c205141e014e8dfa0
Alex Freedman <48782893+axllency@users.noreply.github.com>
2020-12-04 01:37:26 -0500

    heroku deployment w server

--- src/app/services/CourseService.ts
+++ src/app/services/CourseService.ts
@@ -2,5 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable() // @ts-ignore
 export class CourseService{
-    findAllCourses = () => fetch('http://localhost:8080/api/courses').then(response => response.json())
+
+    //findAllCourses = () => fetch('http://localhost:8080/api/courses').then(response => response.json())
+    findAllCourses = () => fetch('http://blackboardapplication-env.eba-8xpppzc9.us-east-2.elasticbeanstalk.com/api/courses').then(response => response.json())
 }
